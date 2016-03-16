3398ff986a5d3b05d45787221d9e9083ba101edc
Kevin Oliva <kevin.oliva@babel.es>
2016-03-16 12:14:03 +0100

    Añadido servir imágenes en estático

--- API_Nodepop/routes/tags.js
+++ API_Nodepop/routes/tags.js
@@ -2,12 +2,31 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Anuncio = mongoose.model("Anuncio");
-var Usuario = mongoose.model("Usuario");
 
 
 
 router.get('/', function(req, res, next) {
-  res.send('Estas en tags');
+
+	var tagsExistentes = [];
+
+	var query = Anuncio.find({});
+	query.exec(function(err, rows){
+		if (err){
+			res.json({result: false, err: err});
+			return;
+		}
+
+		var i = 0;
+		for(i = 0; i<rows.length; i++){
+			console.log(rows[i].tags);
+			tagsExistentes = tagsExistentes.concat(rows[i].tags);
+		}
+
+		var resultadoSinRepetir = tagsExistentes.filter(function (item, pos) {return tagsExistentes.indexOf(item) == pos});
+
+		res.json({result: true, rows: resultadoSinRepetir});
+		return;
+	});
 });
 
 module.exports = router;
